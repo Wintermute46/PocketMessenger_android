3a17a8d87f8b2a34e0ed28f3ffb764d3642b1321
Wintermute46 <r.tzymbalyuk@yandex.ru>
2018-08-12 20:13:38 +0300

    initial commit

---
+++ app/src/main/java/com/gb/pocketmessenger/MainActivity.java
@@ -0,0 +1,27 @@
+package com.gb.pocketmessenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (checkingToken()) {
+            Intent intent = new Intent(this, ChatActivity.class);
+            startActivity(intent);
+        } else {
+            //TODO:  Authorization and Registration fragments
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+    //TODO:  check token
+    private boolean checkingToken() {
+        return true;
+    }
+
+}
