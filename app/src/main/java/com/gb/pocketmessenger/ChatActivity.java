361c897e51e746068a4b6534c04b8b199824c31e
Alexander Korovin <3253340@gmail.com>
2018-08-13 23:37:54 +0700

    Реализованы пустышки фреймов. Начата работа над активностью настроек.

--- app/src/main/java/com/gb/pocketmessenger/ChatActivity.java
+++ app/src/main/java/com/gb/pocketmessenger/ChatActivity.java
@@ -90,6 +90,7 @@ public class ChatActivity extends AppCompatActivity
         if (id == R.id.nav_my_profile) {
             fragmentClass = MyProfileFragment.class;
         } else if (id == R.id.nav_contacts) {
+            //TODO add contacts tab
             return true;
         } else if (id == R.id.nav_support) {
             fragmentClass = SupportFragment.class;
@@ -100,7 +101,7 @@ public class ChatActivity extends AppCompatActivity
             return true;
         }
         try {
-            fragment = (Fragment) fragmentClass.newInstance();
+            fragment = (Fragment) (fragmentClass != null ? fragmentClass.newInstance() : null);
         } catch (Exception e) {
             e.printStackTrace();
         }
